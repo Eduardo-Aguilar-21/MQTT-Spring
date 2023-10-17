9a525ef02fe6efdf2d3d58d92a78da2956d6929a
Eduardo Aguilar <es123y123@gmail.com>
2023-10-17 11:47:05 -0500

    MQTT Json

--- src/main/java/com/ICM/MQTT/MqttSubscriber.java
+++ src/main/java/com/ICM/MQTT/MqttSubscriber.java
@@ -1,5 +1,10 @@
 package com.ICM.MQTT;
 
+import com.ICM.MQTT.Models.UsuariosModel;
+import com.ICM.MQTT.Repositories.UsuariosRepository;
+import com.ICM.MQTT.Services.UsuariosService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -9,10 +14,31 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MqttSubscriber {
+    @Autowired
+    private UsuariosService usuariosService;
+    @Autowired
+    private UsuariosRepository usuariosRepository;
 
     @Autowired
     private IMqttClient mqttClient;
 
+    /*
+        public void subscribeToTopic(String topic) {
+            try {
+                mqttClient.subscribe(topic, new IMqttMessageListener() {
+                    @Override
+                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                        String payload = new String(message.getPayload());
+                        System.out.println("Mensaje MQTT recibido en el tema " + topic + ": " + payload);
+                        // Procesa el mensaje según tus necesidades
+                    }
+                });
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+
+     */
     public void subscribeToTopic(String topic) {
         try {
             mqttClient.subscribe(topic, new IMqttMessageListener() {
@@ -20,7 +46,17 @@ public class MqttSubscriber {
                 public void messageArrived(String topic, MqttMessage message) throws Exception {
                     String payload = new String(message.getPayload());
                     System.out.println("Mensaje MQTT recibido en el tema " + topic + ": " + payload);
-                    // Procesa el mensaje según tus necesidades
+
+                    ObjectMapper objectMapper = new ObjectMapper();
+
+                    try {
+                        UsuariosModel usuariosModel = objectMapper.readValue(payload, UsuariosModel.class);
+
+                        // Guarda el objeto en la base de datos utilizando el servicio
+                        usuariosService.CreateUser(usuariosModel);
+                    } catch (JsonProcessingException e) {
+                        System.err.println("Error al deserializar el JSON: " + e.getMessage());
+                    }
                 }
             });
         } catch (MqttException e) {
@@ -28,3 +64,4 @@ public class MqttSubscriber {
         }
     }
 }
+
